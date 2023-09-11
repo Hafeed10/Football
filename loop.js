ae414c1969a578389a16508044d378294b6abc4d
YourGitHubUsername <muhammedhafeed222@gmail.com>
2023-09-11 08:22:43 +0530

    first commit

---
+++ loop.js
@@ -0,0 +1,12 @@
+function addClass() {
+    document.body.classList.add("sent");
+  }
+  
+  sendLetter.addEventListener("click", addClass);
+
+  const navToggle = document.querySelector(".navbar_toggle");
+  const links = document.querySelector(".main_nav");
+
+ navToggle.addEventListener('click', function(){
+ links.classList.toggle("show_nav");
+})
